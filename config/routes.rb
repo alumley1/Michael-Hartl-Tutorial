bc952c01a785fb598b73be6ad11317afe53a9832
phil303 <philaquilina@gmail.com>
2011-09-21 22:13:49 -0700

    Added tests for view responses and their titles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MichaelHartl::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
