3b28cc13e7ad994b8e0909327ca2574c2eb8d2c7
phil303 <philaquilina@gmail.com>
2011-09-25 13:56:49 -0700

    created User validations and updated respective unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, :presence => true,
+                   :length => { :maximum => 50 },
+                   :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true,
+                    :format => { :with => email_regex }
 end
