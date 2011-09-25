3eba90b2d4f72e49b45810f1893259e17ae51271
phil303 <philaquilina@gmail.com>
2011-09-25 13:59:29 -0700

    Added uniqueness at the database level. IMPORTANT

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110925070530) do
+ActiveRecord::Schema.define(:version => 20110925205722) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20110925070530) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
