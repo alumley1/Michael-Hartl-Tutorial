273351f43d7697bd9ff3d395a719834ff8f90220
phil303 <philaquilina@gmail.com>
2011-09-26 19:12:32 -0700

    Added gem 'Factory Girl', which creates new objects for use in tests.  Created 'show' tests in users_controller_spec

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
+
+Factory.define :user do |user|
+  user.name                   "Philly"
+  user.email                  "philly@gmail.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
