af06e5d02788aaaa63d2eb1c9389c1700651211c
phil303 <philaquilina@gmail.com>
2011-09-30 12:46:03 -0700

    Began testing specs for sign in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
+    @title = "Sign in"
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ MichaelHartl::Application.routes.draw do
   get "sessions/new"
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # this automatically creates named routes in the controller/views
   match '/contact', :to => 'pages#contact'
