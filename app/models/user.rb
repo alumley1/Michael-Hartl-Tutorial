6a470ebd680e0ec20b2160fb1ac56127b5e54c8e
phil303 <philaquilina@gmail.com>
2011-10-01 20:57:30 -0700

    Added schema to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
+
 require 'digest'
 
 class User < ActiveRecord::Base
