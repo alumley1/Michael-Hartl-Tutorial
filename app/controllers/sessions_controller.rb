17d5db80cfeca548f5b4b80d598fcd5f507aeeb4
phil303 <philaquilina@gmail.com>
2011-10-02 11:28:43 -0700

    Set up controller and tests for successful signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination"
+      @title = "Sign in"
+      render 'new'
+    else
+      sign_in user
+      redirect_to user
+    end
   end
 
   def destroy
