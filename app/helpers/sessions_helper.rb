3339163a0eba41131d902e1cb85696df34fdd754
phil303 <philaquilina@gmail.com>
2011-10-02 18:35:56 -0700

    Implemented session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,9 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+#     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:user_id] = user.id
+    self.current_user = user
   end
   
   def current_user=(user)
@@ -10,7 +11,8 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= user_from_remember_token
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+#     @current_user ||= user_from_remember_token
   end
 
   def signed_in?
@@ -18,17 +20,18 @@ module SessionsHelper
   end
 
   def sign_out
-    cookies.delete(:remember_token)
-    current_user = nil
+#     cookies.delete(:remember_token)
+    session[:user_id] = nil
+    self.current_user = nil
   end
 
   private
     
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+#     def user_from_remember_token
+#       User.authenticate_with_salt(*remember_token)
+#     end
 
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
-    end
+#     def remember_token
+#       cookies.signed[:remember_token] || [nil, nil]
+#     end
 end
