91597100b8b5189845bdb8ead2a6b77d37fb8572
phil303 <philaquilina@gmail.com>
2011-10-05 12:09:04 -0700

    added tests for non-signe in users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -209,4 +209,24 @@ describe UsersController do
       end
     end
   end
+
+  describe "authentication of edit/update pages" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    describe "for non-signed-in users" do
+
+      it "should deny access to 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(signin_path)
+      end
+
+      it "should deny access to 'update'" do
+        put :update, :id => @user, :user => {} 
+        response.should redirect_to(signin_path)
+      end
+    end
+  end
 end
