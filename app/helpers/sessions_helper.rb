ea51b96768d2bfa3265b7a393920d2eff3358f0c
phil303 <philaquilina@gmail.com>
2011-10-06 12:19:56 -0700

    Added redirect helper methods to sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,6 +27,20 @@ module SessionsHelper
   end
 
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
   end
 end
