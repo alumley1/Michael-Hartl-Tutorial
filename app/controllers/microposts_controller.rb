2da17e971461ccc0623b148c235b894db93f0546
phil303 <philaquilina@gmail.com>
2011-10-11 12:48:15 -0700

    Finished create action and relevant tests

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,13 @@ class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render 'pages/home'
+    end
   end
 
   def destroy
--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "MicropostsController" do
+describe MicropostsController do
   render_views
 
   describe "access control" do
@@ -13,6 +13,54 @@ describe "MicropostsController" do
     it "should deny access to 'destroy'" do
       post :destroy, :id => 1
       response.should redirect_to(signin_path)
+    end
+  end
+
+  describe "POST 'create'" do
+    
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+    end
+
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :content => "" }
+      end
+
+      it "should not create a micropost" do
+        lambda do
+          post :create, :micropost => @attr
+        end.should_not change(Micropost, :count)
+      end
+
+      it "should render the home page" do
+        post :create, :micropost => @attr
+        response.should render_template('pages/home')
+      end
+    end
+    
+    describe "success" do
+
+      before(:each) do
+        @attr = { :content => "Lorem ipsum" }
+      end
+
+      it "should create a micropost" do
+        lambda do
+        post :create, :micropost => @attr
+        end.should change(Micropost, :count).by(1)
+      end
+      
+      it "should redirect to the home page" do
+        post :create, :micropost => @attr
+        response.should redirect_to(root_path)
+      end
+
+      it "should have a flash message" do
+        post :create, :micropost => @attr
+        flash[:success].should =~ /micropost created/i
+      end
     end
   end
 end
