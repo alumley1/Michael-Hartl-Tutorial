70837b25ef28cf18a0295b698e0889b061ddec07
phil303 <philaquilina@gmail.com>
2011-10-10 18:54:35 -0700

    Created associations b/w user and microposts models.  Relevant tests

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,4 +11,6 @@
 
 class Micropost < ActiveRecord::Base
   attr_accessible :content
+
+  belongs_to :user
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -12,5 +12,29 @@
 require 'spec_helper'
 
 describe Micropost do
-#   pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = Factory(:user)
+    @attr = { :content => "value for content" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    @user.microposts.create!(@attr)
+  end
+
+  describe "user associations" do
+    
+    before(:each) do
+      @micropost = @user.microposts.create(@attr)
+    end
+
+    it "should have a user attribute" do
+      @micropost.should respond_to(:user)
+    end
+
+    it "should have the right associated user" do
+      @micropost.user_id.should == @user.id
+      @micropost.user.should == @user
+    end
+  end
 end
