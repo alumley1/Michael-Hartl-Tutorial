49de5771ab06623bd23b84d7a8252febbf6fb16b
phil303 <philaquilina@gmail.com>
2011-10-11 12:14:35 -0700

    Updated guardfile to ignore routing. Updated routes to include microposts actions create, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 MichaelHartl::Application.routes.draw do
-  get "sessions/new"
-
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
 
   match '/signup', :to => 'users#new'
   match '/signin', :to => 'sessions#new'
