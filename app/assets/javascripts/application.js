bee7049e7942a89b3d4203f33c468e3dd1de8447
phil303 <philaquilina@gmail.com>
2011-10-13 19:43:33 -0700

    Exercise 11.1, added character counter using jQuery complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$('#micropost_content').live('keyup keydown', function(e) {
+  var maxLen = 140;
+  var left = maxLen - $(this).val().length;
+  $('#char-count').html(left);
+});
