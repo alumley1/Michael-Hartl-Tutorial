36c1d726e5d75621b082b70e26b9d18892b0c9e8
Phil <philaquilina@gmail.com>
2011-10-21 12:24:09 -0700

    Implemented feed with more SQL syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,8 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
 
+  scope :admin, where(:admin => true)
+
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
